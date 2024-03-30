e0cd24b689f8c74dea95672461befd89b5c12e25
Abhishek <Abhishek.sidana@daimler.com>
2024-03-30 10:24:12 +0000

    initial setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from './shared/navbar/navbar.jsx';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
